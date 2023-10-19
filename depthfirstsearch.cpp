9d6d709e9e4f0d3190b6dbe0e5e4fff2b7ab7ba3
Marcelo Cohen <mflashbr@gmail.com>
2023-10-19 09:04:47 -0300

    Implementação de DFS (C++)

--- depthfirstsearch.cpp
+++ depthfirstsearch.cpp
@@ -6,23 +6,39 @@
 
 using namespace std;
 
-DepthFirstSearch::DepthFirstSearch(Graph g, string s)
+DepthFirstSearch::DepthFirstSearch(Graph &g, string s)
 {
 	this->s = s;
 	dfs(g, s);
 }
 
-void DepthFirstSearch::dfs(Graph g, string s)
+void DepthFirstSearch::dfs(Graph &g, string v)
 {
+	// cout << "em " << v << endl;
+	marked.insert(v); // marca v como visitado
+	for (auto const &w : g.getAdj(v))
+	{
+		if (marked.find(w) == marked.end())
+		{
+			edgeTo[w] = v;
+			dfs(g, w);
+		}
+	}
 }
 
 bool DepthFirstSearch::hasPathTo(string s)
 {
+	return marked.find(s) != marked.end();
 }
 
-vector<string> DepthFirstSearch::pathTo(string s)
+vector<string> DepthFirstSearch::pathTo(string v)
 {
 	vector<string> path;
-	// ...
+	while (v != s)
+	{
+		path.insert(path.begin(), v);
+		v = edgeTo[v];
+	}
+	path.insert(path.begin(), s);
 	return path;
 }
--- depthfirstsearch.h
+++ depthfirstsearch.h
@@ -11,16 +11,16 @@ class DepthFirstSearch
 {
 
 public:
-	DepthFirstSearch(Graph g, std::string s);
+	DepthFirstSearch(Graph &g, std::string s);
 
-	bool hasPathTo(std::string s);
-	std::vector<std::string> pathTo(std::string s);
+	bool hasPathTo(std::string v);
+	std::vector<std::string> pathTo(std::string v);
 
 private:
 	std::string s;
 	std::set<std::string> marked;
 	std::map<std::string, std::string> edgeTo;
-	void dfs(Graph g, std::string s);
+	void dfs(Graph &g, std::string v);
 };
 
 #endif
