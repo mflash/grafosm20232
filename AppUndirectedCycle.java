cad066cc3ce5787c389afcb8e3c55ea2000250b8
Marcelo Cohen <mflashbr@gmail.com>
2023-10-24 09:21:07 -0300

    Exemplo adicional de grafo não dirigido com ciclo

--- AppUndirectedCycle.java
+++ AppUndirectedCycle.java
@@ -1,7 +1,9 @@
 public class AppUndirectedCycle {
 
     public static void main(String[] args) {
-        Graph g = new Graph("tinyG.txt");
+        Graph g = new Graph("ciclonaodirigido.txt");
+
+        System.out.println(g.toDot());
 
         UndirectedCycle uc = new UndirectedCycle(g);
         System.out.println(uc.containsCycle());
